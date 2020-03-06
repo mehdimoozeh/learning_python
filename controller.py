59df3ff1aa11bfaa3b1c0ce8dcda3b2a5f9157e6
Mehdi <mohammadmoozeh@gmail.com>
2020-03-06 17:02:15 +0330

    feat: add navbar

--- controller.py
+++ controller.py
@@ -5,12 +5,13 @@ urls = (
     '/', 'home'
 )
 
+render = web.template.render('Views/Templates/', base="MainLayout")
 app = web.application(urls, globals())
 
 # Classes / Routes
 class home:
     def GET(self):
-        return "home"
+        return render.Home()
 
 if __name__ == "__main__":
     app.run()
