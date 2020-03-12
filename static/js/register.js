ed276bf228eb97ac69fd5ded23aa10992b98afdc
Mehdi <mohammadmoozeh@gmail.com>
2020-03-12 18:11:24 +0330

    feat: show correct response to user after register

--- static/js/register.js
+++ static/js/register.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
         $(document).on('submit', '#register_form', function(e) {
         e.preventDefault();
         let form = $("#register_form").serialize()
-        console.log("form: ", form);
         $.ajax({
             url: '/register',
             type: 'POST',
             data: form,
             success: function(response){
-                console.log(response);
+                $("#result").html("User created successfully, user Id: " + response)
+                $("#result").css("visibility", "visible")
             }
         })
     })
