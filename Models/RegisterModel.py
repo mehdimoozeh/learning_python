e2a05e441316c4d90a7137e2721faea6e92276b2
Mehdi <mohammadmoozeh@gmail.com>
2020-03-12 18:14:32 +0330

    feat: add fullname into register page

--- Models/RegisterModel.py
+++ Models/RegisterModel.py
@@ -7,6 +7,6 @@ class RegisterModel:
         self.db = self.client.sampleWeb
         self.Users = self.db.users
 
-    def insert_user(self, username, password):
-        _id = self.Users.insert({"username": username, "password": password})
+    def insert_user(self, username, password, fullname):
+        _id = self.Users.insert({"username": username, "password": password, "fullname": fullname})
         return _id
--- controller.py
+++ controller.py
@@ -23,7 +23,7 @@ class Register:
         data = web.input()
         registration = RegisterModel.RegisterModel()
         hashed_password = bcrypt.hashpw(data.password.encode(), bcrypt.gensalt())
-        _id = registration.insert_user(data.username, hashed_password)
+        _id = registration.insert_user(data.username, hashed_password, data.fullname)
         return _id
 
 if __name__ == "__main__":
